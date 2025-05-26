b5108fba78dae0a6a15aa4b3a968965e6f165e48
Yilin <yilin.zh135@gmail.com>
2025-05-26 09:41:10 +0200

    Add home components

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React from "react";
+import greenWave from "../../assets/images/green_wave.svg";
+import whiteWave from "../../assets/images/white_wave.svg";
+import brownWave from "../../assets/images/brown_wave.svg";
 
-function HomeContent( children, bgColor  ) {
-  return (
-    <section
-      style={{
-        backgroundColor: bgColor,
-        padding: "2rem",
-        color: bgColor === "white" ? "#000" : "#fff",
-      }}
-    >
-      {children}
-    </section>
-  );
+import "./HomeContent.css";
+
+function HomeContent({ id, bgColor, subtitle, title, text,image, isLast }) {
+    const waveSrc = isLast
+        ? brownWave
+        : bgColor === "var(--light_green)"
+        ? whiteWave
+        : greenWave;
+
+    const dynamicClass = `home-content-container home-content-${id}`;
+
+    return (
+        <div
+            className={dynamicClass}
+            style={{
+                backgroundColor: bgColor,
+                color: bgColor === "white" ? "#000" : "#fff",
+            }}
+        >
+            <div className="home_content_text">
+                <h3>{subtitle}</h3>
+                <h2>{title}</h2>
+                <p>{text}</p>
+            </div>
+            <div className="home_content_img">
+                 {image && <img src={image} alt={`${title} illustration`} className="section-image" />}
+            </div>
+
+            <img
+                className="home_content_wave"
+                src={waveSrc}
+                alt="Decorative wave"
+            />
+        </div>
+    );
 }
 
-export default HomeContent
+export default HomeContent;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,42 @@ import { useState } from "react";
 import { FaHome, FaPlus } from "react-icons/fa";
 import "./Home.css";
 import whiteWave from "../../assets/images/white_wave.svg";
-import greenWave from "../../assets/images/green_wave.svg";
+import HomeContent from "../../components/HomeContent/HomeContent.jsx";
 
+import sectionsData from "../../mocks/sectionsData.json";
+import taskScreen from "../../assets/images/task_screen.png"; // Import real
+
+const sectionsDataWithImages = sectionsData.map((section) => {
+    if (section.id === 1) {
+        return { ...section, image: taskScreen };
+    }
+    return section;
+});
 function Home() {
     return (
         <div className="home-container">
             <div className="hero">
-                 <img className="hero_wave" src={whiteWave} alt="Decorative wave" />
+                <img
+                    className="hero_wave"
+                    src={whiteWave}
+                    alt="Decorative wave"
+                />
             </div>
-            <div className="home-content-container">
-                 <img className="home_content_wave" src={greenWave} alt="Decorative wave" />
-
+            <div>
+                {sectionsDataWithImages.map(
+                    ({ id, bgColor, subtitle, title, text, image }, index) => (
+                        <HomeContent
+                            key={id}
+                            id={id}
+                            bgColor={bgColor}
+                            subtitle={subtitle}
+                            title={title}
+                            text={text}
+                            image={image}
+                            isLast={index === sectionsDataWithImages.length - 1}
+                        />
+                    )
+                )}
             </div>
         </div>
     );
